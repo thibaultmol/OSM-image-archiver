ce56fedb6bca423c8f28214c548e4aba306e03f5
Thibault Molleman <thibaultmol@users.noreply.github.com>
2023-04-21 10:21:47 +0000

    Script to process/download the txt url file

---
+++ Download images via IMGURuls.txt file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+download_file() {
+  url="$1"
+  file="$(basename "$url")"
+  if [ ! -f "$file" ]; then
+    echo "Downloading $url"
+    curl -O "$url"
+  else
+    echo "Skipping $file, already downloaded"
+  fi
+}
+
+export -f download_file
+
+while read -r url; do
+  while [ "$(jobs | wc -l)" -ge 5 ]; do
+    sleep 1
+  done
+
+  download_file "$url" &
+done < urls.txt
+wait
+
+echo "All downloads complete"

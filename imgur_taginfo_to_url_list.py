def4110cdd06887618ff2008324b5df0bbaabaea
Thibault Molleman <thibaultmol@users.noreply.github.com>
2023-10-21 20:21:18 +0200

    fix actions

--- imgur_taginfo_to_url_list.py
+++ imgur_taginfo_to_url_list.py
@@ -3,6 +3,29 @@
 import requests
 import re
 
+from datetime import datetime
+
+def save_imgur_urls(imgur_urls, output_filename, all_output_filename):
+    """Save the extracted Imgur URLs to a text file."""
+    with open(output_filename, 'w', encoding='utf-8') as output_file:
+        for url in imgur_urls:
+            output_file.write(f"{url}\n")
+
+    # Append to All_Imgur_urls.txt and remove duplicates
+    all_urls = set()
+    try:
+        with open(all_output_filename, 'r', encoding='utf-8') as all_output_file:
+            all_urls = set(line.strip() for line in all_output_file)
+    except FileNotFoundError:
+        pass
+
+    all_urls.update(imgur_urls)
+    
+    with open(all_output_filename, 'w', encoding='utf-8') as all_output_file:
+        for url in all_urls:
+            all_output_file.write(f"{url}\n")
+
+
 def download_json(url):
     """Download JSON data from a URL."""
     response = requests.get(url, timeout=10)
@@ -39,7 +62,10 @@ def main():
         "https://taginfo.openstreetmap.org/api/4/search/by_value?"
         "query=imgur&sortname=count_all&sortorder=desc&rp=999&page="
     )
-    output_filename = "IMGURurls.txt"
+
+    # Define the filename using the current date
+    output_filename = f"Snapshots/{datetime.now().strftime('%Y-%m-%d')}.txt"
+    all_output_filename = "All_Imgur_urls.txt"
 
     imgur_urls = []
     page = 1
@@ -54,9 +80,10 @@ def main():
             break
         page += 1
 
-    print("Saving Imgur URLs to IMGURurls.txt...")
-    save_imgur_urls(imgur_urls, output_filename)
-    print("Done! Imgur URLs saved in IMGURurls.txt")
+    print(f"Saving Imgur URLs to {output_filename}...")
+    save_imgur_urls(imgur_urls, output_filename, all_output_filename)
+    print(f"Done! Imgur URLs saved in {output_filename}")
+
 
 if __name__ == "__main__":
     main()
